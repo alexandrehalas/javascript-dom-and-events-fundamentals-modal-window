9f7e5fa42f36d8e2138cac145aefad49c701af86
Alexandre Halas <alexandrehalas@gmail.com>
2023-06-22 17:13:19 -0300

    Adding in variables all the classes that will be used

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnsShowModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+
+console.log(btnsShowModal);
+
+for (let i = 0; i < btnsShowModal.length; i++) {
+  console.log(btnsShowModal[i].textContent);
+}
